c5c440894058c83496177e2794e719b5511e3700
Kasper Riis Pedersen <kaspermail1999@gmail.com>
2019-11-12 11:48:07 +0100

    Initial commit

---
+++ rentals/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Rental(models.Model):
+    id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    bedrooms = models.IntegerField()
+    image = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
